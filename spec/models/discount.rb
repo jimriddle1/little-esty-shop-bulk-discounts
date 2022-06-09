5ed7b44bdcba4b146cb59afd6228519011c03b7e
Jim Riddle <jimriddle0@gmail.com>
2022-06-09 13:27:51 -0500

    tested and setup relationships between merchant and discount

--- spec/models/discount.rb
+++ spec/models/discount.rb
@@ -3,6 +3,5 @@ require 'rails_helper'
 RSpec.describe Discount do
     describe "relationships" do
         it { should belong_to :merchant }
-        it { should have_many(:transactions).through(:invoices) }
     end
 end
